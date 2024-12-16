10d7129552a98dc60091d146d86699c60f528a8c
elshora97 <A.elshora@dataserve.com.sa>
2024-12-16 23:06:55 +0200

    Add DB Connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 const app = express();
 
-app.listen(5000, () => {
-  console.log("app is running");
+const port = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, async () => {
+  await connectDB();
+  console.log(`app is running at port ${port}`);
 });
