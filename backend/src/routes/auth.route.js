eb3a7d4eb1bf937063b969157d09599388f0a0f5
elshora97 <A.elshora@dataserve.com.sa>
2024-12-18 00:44:05 +0200

    Add Update Profile Controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { signup, login, logout } from "../controller/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+  checkAuth,
+} from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +15,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
